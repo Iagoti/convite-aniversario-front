ca2af7e33845b08b4f32a69132a477346047481c
Iagoti <iago@tivic.com.br>
2026-06-29 05:04:36 -0300

    Add location

--- src/app/core/services/evento.service.ts
+++ src/app/core/services/evento.service.ts
@@ -16,7 +16,7 @@ export class EventoService {
     mensagem: 'Espero você para comemorar comigo esse dia tão especial!',
     localFesta: 'Salão de Festas Encanto',
     endereco: 'Rua das Flores, 123 - Centro',
-    linkMaps: 'https://maps.google.com',
+    linkMaps: 'https://maps.app.goo.gl/HkSEvZLeTbSQt5nX7',
     videoUrl: 'assets/videos/convite-isabella.mp4',
   };
 
